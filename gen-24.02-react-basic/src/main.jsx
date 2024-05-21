db7b134d91fbbc4a27ce8b7c978dbf9608897ed1
yusufva <yusufvalent@gmail.com>
2024-05-21 10:49:29 +0700

    create tugas react basic

---
+++ gen-24.02-react-basic/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ProductPages from "./Pages/products.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ProductPages />
+    </React.StrictMode>
+);
