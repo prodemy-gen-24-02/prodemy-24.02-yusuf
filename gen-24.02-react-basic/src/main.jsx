95b6307c7fd32c17361b84040ba9377dc6db2105
yusufva <yusufvalent@gmail.com>
2024-05-22 07:59:24 +0700

    modify main

--- gen-24.02-react-basic/src/main.jsx
+++ gen-24.02-react-basic/src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import ProductPages from "./Pages/products.jsx";
 import "./index.css";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <ProductPages />,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <ProductPages />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
