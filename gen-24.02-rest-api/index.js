659cdec71d3f6f962c7ed24d58c88614c48d7ce2
yusufva <yusufvalent@gmail.com>
2024-05-29 10:10:03 +0700

    adding more function

--- gen-24.02-rest-api/index.js
+++ gen-24.02-rest-api/index.js
@@ -2,11 +2,35 @@ import axios from "axios";
 
 const url = "https://dummyjson.com/products";
 
-axios
-    .get(url)
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((error) => {
-        console.log(error);
+const getProduct = async () =>
+    await axios
+        .get(url)
+        .then((response) => {
+            console.log("products:");
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+// getProduct();
+
+let id = 1;
+
+const addProduct = async () => {
+    const response = await axios.post("https://dummyjson.com/products/add", {
+        title: "Gachapon",
     });
+    console.log(response.data);
+};
+
+addProduct();
+
+const updateProduct = (id) => {
+    const response = axios.put(`https://dummyjson.com/products/${id}`, {
+        title: "Gachapon edit",
+    });
+    console.log(response.data);
+};
+
+updateProduct(id);
